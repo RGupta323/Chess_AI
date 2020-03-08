b40977478d38340393e0d8670b5ecebf900e5346
Rohan Gupta <guptarohan323@gmail.com>
2020-03-08 13:25:12 -0500

    Modifications to Pawn, Rook classes. Created

--- src/Knight.java
+++ src/Knight.java
@@ -1,29 +1,31 @@
 import java.util.LinkedList;
 
-public class Rook extends ChessPiece{
-    int x_pos;
-    int y_pos;
-    String color;
-    boolean captured;
-    String type;
-    LinkedList<ChessPosition<Integer>> moves;
-    public Rook(String color, int x_pos, int y_pos){
+public class Knight extends ChessPiece{
+    //instance variables
+        int x_pos;
+        int y_pos;
+        String color;
+        boolean captured;
+        LinkedList<ChessPosition<Integer>> moves;
+        String type;
+    public Knight(String color, int x_pos, int y_pos){
         this.color=color;
         this.x_pos=x_pos;
         this.y_pos=y_pos;
-        type="Rook";
+        type="Knight";
         captured=false;
         moves=new LinkedList<>();
     }
     public void move(int x_pos1, int y_pos1){
-        x_pos=x_pos1;
-        y_pos=y_pos1;
+
     }
+
     public void updateMoves(Integer x_pos, Integer y_pos){
         moves.add(new ChessPosition(x_pos,y_pos));
     }
+
     @Override
     public String toString() {
-        return color+" "+"Rook";
+        return color+" "+"Knight";
     }
 }
--- src/Pawn.java
+++ src/Pawn.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public class Pawn extends ChessPiece {
     String color; int x_pos; int y_pos;
-    boolean notMoved; String type;
+    boolean notMoved; String type; boolean captured;
     List<ChessPosition<Integer>> moves;
     public Pawn(String color, int x_pos, int y_pos) {
         this.color = color;
@@ -13,45 +13,43 @@ public class Pawn extends ChessPiece {
         notMoved=true;
         moves=new LinkedList<>();
         type=toString();
+        captured=false;
+
     }
     //methods
 
     //move(), method that verifies if a pawn has legal move and updates its current position.
     public void move(int x_pos1, int y_pos1)throws Exception{
         //check if its a legal move.
-        if(notMoved){
-            if((x_pos+1==x_pos1 || x_pos+2==x_pos1) && (y_pos1==y_pos)){
-                x_pos=x_pos1;
-                y_pos=y_pos1;
-                notMoved=false;
-            }
-            else{
-                throw new Exception ("Illegal Move, Pawn");
-            }
+        if(isLegal(x_pos1, y_pos1)) {
+            x_pos = x_pos1;
+            y_pos = y_pos1;
+        }
+        else throw new Exception("Illegal move."); 
+    }
+    //method to check if a position is a legal one for the piece
+    public boolean isLegal(int x_pos, int y_pos){
+        if(color=="black") {
+            if(notMoved)  return ((x_pos == this.x_pos - 1 && y_pos==this.y_pos) || (x_pos==this.x_pos-2 && y_pos==this.y_pos) || (x_pos==this.x_pos-1 && y_pos==this.y_pos-1) || (x_pos==this.x_pos-1 && y_pos==this.y_pos+1));
+            return ((x_pos == this.x_pos - 1 && y_pos==this.y_pos) || (x_pos==this.x_pos-1 && y_pos==this.y_pos-1) || (x_pos==this.x_pos-1 && y_pos==this.y_pos+1));
         }
         else{
-            if(x_pos+1==x_pos1 && y_pos1==y_pos){
-                x_pos=x_pos1;
-                y_pos=y_pos1;
-            }
-            else{
-                throw new Exception("Illegal move, Pawn");
-            }
+            return false;
         }
-        x_pos=x_pos1; y_pos=y_pos1;
     }
+
     //options (creates a data structure of options grid where the piece can move based on the its original coordaintes
 
     //make drawPath(), maps the path in which the pawn takes. (this will be used to draw with the GUI)
     public void updateMoves(Integer x_pos, Integer y_pos){
-        List<ChessPosition<Integer>> a = new LinkedList<>();
+        //List<ChessPosition<Integer>> a = new LinkedList<>();
         ChessPosition<Integer> c = new ChessPosition(x_pos, y_pos);
-        a.add(c);
+        moves.add(c);
 
     }
 
     //methods for capture
-    public boolean canCapture(String[][] board){
+    /*public boolean canCapture(String[][] board){
        if(color=="black" || color.equals("black")){
            //pawns can capture diagonally, but black pawns are located at the end
            //of the board, so
@@ -61,13 +59,26 @@ public class Pawn extends ChessPiece {
            ChessPosition<Integer> diagonal_left=new ChessPosition(x_pos-1, y_pos-1);
            ChessPosition<Integer> diagonal_right=new ChessPosition<>(x_pos+1, y_pos+1);
 
-           if(diagonal_left.x>=0 && diagonal_left.x<8){
-               
+           if((diagonal_left.x>=0 && diagonal_left.x<8) || (diagonal_left.y>=0 && diagonal_left.y<8)){
+               return true;
            }
 
+           if((diagonal_right.x>=0 && diagonal_right.x<8) || (diagonal_right.y>=0 && diagonal_right.y<8)){
+               return true;
+           }
        }
+       if(color=="white" || color.equals("white")){
+           //TODO
+       }
+
+
+       return false;
     }
 
+    //more todo methods
+    public static List<List<ChessPosition<Integer>>> createOptionsGrid(String[] board){
+        //TODO
+    }*/
     //toString method to show what type it is...
     public String toString(){
         return "Pawn";
